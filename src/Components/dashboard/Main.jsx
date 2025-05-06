1abf363de3fd472737b66b46a830cbfbe553fff1
Sarfraz1115 <syedsarfraz907@gmail.com>
2025-05-06 15:39:37 +0530

    Third commit

--- src/Components/dashboard/Main.jsx
+++ src/Components/dashboard/Main.jsx
@@ -4,9 +4,8 @@ import CardSummary from './CardSummary'
 import Addtask from './Addtask'
 import Filtertask from './Filtertask'
 import { toast } from 'react-toastify'
-import { Edit, Trash2 } from 'lucide-react'
 import { supabase } from '../../utils/supabaseClient'
-import { useNavigate } from 'react-router-dom'
+import { data, useNavigate } from 'react-router-dom'
 import Tasklists from './Tasklists'
 
 const Main = () => {
@@ -17,14 +16,14 @@ const Main = () => {
 
     useEffect(() => {
         const checkAuth = async () => {
-            const {data: {user}} = await supabase.auth.getUser();
-            if(!user){
+            const { data: { user } } = await supabase.auth.getUser();
+            if (!user) {
                 alert("Please Login to see your tasks");
-                navigate("/login", {replace: true});
+                navigate("/login", { replace: true });
             }
         };
         checkAuth();
-    },[navigate])
+    }, [navigate])
 
     useEffect(() => {
         const fetchtasks = async () => {
@@ -35,6 +34,7 @@ const Main = () => {
                 return;
             }
             const formatted = data.map((task) => ({
+                id: task.id,
                 title: task.title,
                 isCompleted: task.is_completed,
             }));
@@ -68,18 +68,18 @@ const Main = () => {
                 toast.error("error in ddatabse", error.message);
                 return;
             }
-            if(data && data.length > 0) {
+            if (data && data.length > 0) {
                 settasks((prev) => [
-                  {
-                    id: data[0].id,
-                    title: taskinput,
-                    isCompleted: false,
-                  },
-                  ...prev
+                    {
+                        id: data[0].id,
+                        title: taskinput,
+                        isCompleted: false,
+                    },
+                    ...prev
                 ]);
                 settaskinput("");
                 toast.success("Task Added Successfully")
-              }
+            }
 
         } catch (err) {
             console.log("Unexpected error0, ", err);
@@ -102,6 +102,48 @@ const Main = () => {
         })
         settasks(updatetask);
     }
+    const handleDelete = async (taskId) => {
+        try {
+            const { data: { user } } = await supabase.auth.getUser();
+            const { error } = await supabase.from("Tasks").delete().eq("id", taskId);
+
+            if (error) {
+                toast.error("Error in deleting task ", error.message);
+                return;
+            }
+            settasks((prev) => prev.filter((task) => task.id !== taskId));
+            toast.success("Task Deleted Successfully")
+
+        } catch (err) {
+            console.error("Unexpected error in handleDelete:", err);
+            toast.error("Unexpected error occurred while deleting the task.");
+        }
+    };
+
+
+    const taskEdit = async (taskId) => {
+        const task = tasks.find((task) => task.id === taskId);
+        if (!task) { toast.error("task not found"); return; }
+        const taskalert = prompt("edit task", task.title);
+        if (taskalert == null || taskalert === "") { toast.error("Please enter a valid task"); return; }
+        try {
+            const { error } = await supabase.from("Tasks").update({ title: taskalert }).eq("id", taskId);
+            if (error) {
+                toast.error("Error in updating task", error.message);
+                return;
+            }
+            settasks((prev) => prev.map((task) => {
+                if (task.id === taskId) {
+                    return { ...task, title: taskalert };
+                }
+                return task;
+            }));
+            toast.success("Task Updated Successfully");
+        } catch (error) {
+            console.error("Unexpected error in taskEdit:", err);
+            toast.error("Unexpected error occurred while editing the task.");
+        }
+    }
 
 
     const totaltasks = tasks.length;
@@ -123,7 +165,7 @@ const Main = () => {
                 <Filtertask className="hover:cursor-pointer" text="Completed" active={filter === 'completed'} count={completedTasks} onClick={() => setfilter('completed')} />
                 <Filtertask className="hover:cursor-pointer" text="Pending" active={filter === 'pending'} count={pendingTask} onClick={() => setfilter('pending')} />
             </div>
-           <Tasklists handledelete={handleDelete} handleedit={handleEdit} tasks={filteredTasks} handleinput={handleinput}/>
+            <Tasklists handledelete={(taskId) => handleDelete(taskId)} handleedit={(taskId) => taskEdit(taskId)} tasks={filteredTasks} handleinput={handleinput} />
         </main>
     )
 }
